fa0de2fca40135b19310649a799105687273e254
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-04 23:46:17 +0530

    Appear once

--- appearOnce.cpp
+++ appearOnce.cpp
@@ -2,16 +2,14 @@
 using namespace std;
 int appearOnce(int arr[],int n){
     int ans=0;
-    for(int i=00;i<n;i++){
+    for(int i=0;i<n;i++){
         ans^=arr[i];
     }
     return ans;
 }
 int main(){
-    int arr[5]={1,1,4,5,5};
+    int arr[5]={1,1,2,3,3};
     int n=5;
-    int ans = appearOnce(arr,n);
-    cout<<ans;
+    cout<<appearOnce(arr,n);
     return 0;
-
 }
