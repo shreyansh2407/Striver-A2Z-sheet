886479b5adec0303ad1df99b72b312559d948134
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-07 00:00:55 +0530

    2 sum problem

---
+++ 2sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int arr[100]={2,6,5,1,11};
+    
+    int  target = 3;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+           
+            
+            if(arr[i]+arr[j]==target){
+                    cout<<"Yes"<<endl;
+                    
+            }
+        }
+        
+    }
+   
+    return 0;
+}
